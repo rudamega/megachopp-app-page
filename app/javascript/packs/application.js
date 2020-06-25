6a2850521cabaf94ad67fdf28a3b51be3ce0daab
ruben medina <ruben@megachopp.com.py>
2020-06-25 15:21:10 -0400

    git

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+import { toolTips } from 'components/tooltips'
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -31,4 +33,6 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  toolTips();
+
 });
