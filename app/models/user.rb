c22be5bd2715145a12027754e0e0ffaad6d04452
ruben medina <ruben@megachopp.com.py>
2020-06-26 14:14:58 -0400

    working mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+after_create :send_welcome_email
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+private
+
+  def send_welcome_email
+    UserMailer.welcome(self).deliver_now
+  end
+
 end
+
+
