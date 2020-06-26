e9acd5a50cc6d8d12fa8ecd1456c41fda8487cb5
ruben medina <ruben@megachopp.com.py>
2020-06-26 11:40:12 -0400

    mailer working

--- Gemfile
+++ Gemfile
@@ -22,7 +22,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'redis', '~> 4.0'
 gem 'dotenv-rails', groups: [:development, :test]
 gem 'cloudinary', '~> 1.12.0'
-gem 'postmark-rails'
+gem 'mail', '~> 2.7', '>= 2.7.1'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,4 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://localhost:3000", port: 3000 }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -33,9 +32,12 @@ Rails.application.configure do
   config.active_storage.service = :cloudinary
 
   #config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.delivery_method = :letter_opener
+
+
+    #config.action_mailer.perform_deliveries = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.default_url_options = { host: "http://localhost:3000", port: 3000 }
+
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
