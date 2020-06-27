679ddbaa450b982aa0a136c82f972bbca8b98231
ruben medina <ruben@megachopp.com.py>
2020-06-27 17:37:42 -0400

    integer

--- db/migrate/20200627023615_create_cars.rb
+++ db/migrate/20200627023615_create_cars.rb
@@ -2,7 +2,7 @@ class CreateCars < ActiveRecord::Migration[6.0]
   def change
     create_table :cars do |t|
       t.string :name
-      t.string :kilometer
+      t.integer :kilometer
       t.string :mantenimiento
 
       t.timestamps
