b1faa2be943b2903d954824f780d1c3c1b1bcc4c
ruben medina <ruben@megachopp.com.py>
2020-06-29 14:44:11 -0400

    test

--- db/migrate/20200629180607_change_kilometer_from_cars.rb
+++ db/migrate/20200629180607_change_kilometer_from_cars.rb
@@ -1,5 +1,5 @@
 class ChangeKilometerFromCars < ActiveRecord::Migration[6.0]
   def change
-     change_column :cars, :kilometer, "USING kilometer::double precision"
+     change_column :cars, :kilometer, :float, precision: 10
   end
 end
